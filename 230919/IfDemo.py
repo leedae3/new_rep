8fc76e5016853d68fd7b8ac32d328f7d8096ff93
sunday <fj2379@gmail.com>
2023-09-20 16:02:10 +0900

    오후 네시 이분에 추가

---
+++ 230919/IfDemo.py
@@ -0,0 +1,24 @@
+# score = int(input("enter a score : "))
+# if score >= 60 :
+#     print("success")
+# else :
+#     print("Failure")
+
+ 
+ 
+# avg = float(input("enter s average : "))
+# if 90 <= avg <= 100 : print("A")
+# else : print("F")
+
+
+season = input("what is your favorite season? : ").lower()
+if season == "spring" : 
+    print("봄")
+elif season == "summer" :
+    print("여름")
+elif season == "fall" : 
+    print("가울")
+elif season == "winter" : 
+    print("겨울")
+else :
+    print("season!")
